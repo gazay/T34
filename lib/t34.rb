61a8941cd5f7548f1e696a32920caa3713557fd7
Vlad Bokov <razum2um@mail.ru>
2013-12-09 17:37:05 +0700

    move rewriter outside

--- lib/t34.rb
+++ lib/t34.rb
@@ -2,33 +2,4 @@ require 'parser/current'
 require 'unparser'
 require File.expand_path("../extended_node.rb", __FILE__)
 
-module T34
-  class Rewriter
-    def initialize(source)
-      @source = source
-    end
-
-    def rewrite(&block)
-      instance_eval(&block)
-    end
-
-    def target
-      Unparser.unparse(ast)
-    end
-
-    def ast
-      @ast ||= Parser::CurrentRuby.parse(@source)
-    end
-
-    def methods(*names)
-      methods = []
-      ast.traverse do |node|
-        if node.method?(*names)
-          methods << node
-          yield node if block_given?
-        end
-      end
-      methods
-    end
-  end
-end
+require 't34/rewriter'
---
+++ lib/t34/rewriter.rb
@@ -0,0 +1,30 @@
+module T34
+  class Rewriter
+    def initialize(source)
+      @source = source
+    end
+
+    def rewrite(&block)
+      instance_eval(&block)
+    end
+
+    def target
+      Unparser.unparse(ast)
+    end
+
+    def ast
+      @ast ||= Parser::CurrentRuby.parse(@source)
+    end
+
+    def methods(*names)
+      methods = []
+      ast.traverse do |node|
+        if node.method?(*names)
+          methods << node
+          yield node if block_given?
+        end
+      end
+      methods
+    end
+  end
+end
